c354a6eb0d18a629c00637f3912f94ae07c39178
NajmeMiri <najme.miri@gmail.com>
2022-11-16 23:27:44 +0100

    addig header component and links to home and shop page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import HomePage from "./pages/homepage/homepage";
 import ShopPage from "./pages/shops/shop";
+import Header from "./components/header/header";
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
