d2f45ffdbaeb3d11fd0f3c61597ba942d058b098
NajmeMiri <najme.miri@gmail.com>
2022-12-05 00:16:36 +0100

    Added in cart item designs and cart count

--- src/components/cart-dropdown/cart-dropdown.jsx
+++ src/components/cart-dropdown/cart-dropdown.jsx
@@ -8,12 +8,10 @@ import "./cart-dropdown.scss";
 const CartDropdown = () => {
 
     const { cartItems } = useContext(CartContext);
-    const renderedItems = cartItems.map(item => <CartItem key={item.id} cartItem={item} />);
-    console.log(cartItems, renderedItems)
     return (
         <div className="cart-dropdown-container">
             <div className="cart-items">
-                {renderedItems}
+                {cartItems.map(item => <CartItem key={item.id} cartItem={item} />)}
             </div>
             <CustomButton>go to checkout</CustomButton>
         </div>
--- src/components/cart-item/cart-item.jsx
+++ src/components/cart-item/cart-item.jsx
@@ -3,11 +3,15 @@ import React from "react";
 import "./cart-item.scss";
 
 const CartItem = ({ cartItem }) => {
-    const { name, quantity } = cartItem;
+    const { name, quantity, imageUrl, price } = cartItem;
     return (
-        <div>
-            <h1>{name}</h1>
-            <span>{quantity}</span>
+        <div className="cart-item-container">
+            <img src={imageUrl} alt={`${name}`} />
+            <div className="item-details">
+                <span className="name">{name}</span>
+                <span className="price">{quantity} x ${price}</span>
+            </div>
+
         </div>
     );
 }
