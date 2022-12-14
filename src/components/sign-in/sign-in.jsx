0f02fec1a05126b5be1e158346583d8e1a2105af
NajmeMiri <najme.miri@gmail.com>
2022-12-14 23:42:36 +0100

    fixed an error in sign in form

--- src/components/sign-in/sign-in.jsx
+++ src/components/sign-in/sign-in.jsx
@@ -55,8 +55,8 @@ const SignInForm = () => {
             <h2>I already have an account</h2>
             <span>Sign in with your email and password</span>
             <form onSubmit={handleSubmit}>
-                <FormInput name="email" handleChange={handleChange} label="email" value={email} required />
-                <FormInput name="password" type="password" handleChange={handleChange} label="password" value={password} required />
+                <FormInput name="email" onChange={handleChange} label="email" value={email} required />
+                <FormInput name="password" type="password" onChange={handleChange} label="password" value={password} required />
                 <div className="buttons-container">
                     <CustomButton type="submit" buttonType={BUTTON_TYPE_CLASSES.base}>SIGN IN</CustomButton>
                     <CustomButton type="button" onClick={signInWithGoogle} buttonType={BUTTON_TYPE_CLASSES.google}> google sign in</CustomButton>
